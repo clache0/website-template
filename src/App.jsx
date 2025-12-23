e61455203f407868f6bb8c83efa5f66f062e6905
Clayton Cheng <clay578@gmail.com>
2025-12-23 14:26:40 -0800

    initial commit: react js

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+      </nav>
+
+      <section id="home">Home</section>
+      <section id="about">About</section>
+      <section id="contact">Contact</section>
+    </>
+  );
+}
+
+export default App;
+
